3504c24faf6c62f1fee40ffbad1d7995cc9649e2
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2023-07-04 23:13:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://ThanzeerCould1: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@github.com/ThanzeerCould1/RequestRepo /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
