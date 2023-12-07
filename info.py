f85a15036d2093870dfd93d9c417b03556632600
MHD X THANZEER <115057527+ThanzeerCould1@users.noreply.github.com>
2023-12-07 21:59:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "6206165162:AAF2npkQYLkYHJY2rebmygFNCehdLVx
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/5dca04109fd6f5523b1e1.jpg').split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/5dca04109fd6f5523b1e1.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1482016419 750066571').split()]
